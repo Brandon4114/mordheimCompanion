e2468268929b2d0946f037724d0e84652ea3a09f
Brandon4114 <brandonh96@live.co.uk>
2023-11-18 16:12:46 +0000

    Create Base App

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mordheimCompanion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mordheimCompanion
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
